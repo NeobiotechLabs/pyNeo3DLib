ebf82a5219146af2f8ea8b87537c4417953963cc
hoki <hk.sim@neobiotech.com>
2025-06-10 17:56:52 +0900

    progress event 정리 및 추가2

--- pyNeo3DLib/registration.py
+++ pyNeo3DLib/registration.py
@@ -56,10 +56,12 @@ class Neo3DRegistration:
     
     async def run_registration(self, visualize=False):       
         self.__verify_file_info()
+        
+        total_progress = 9
 
         data = {
             "type": "progress",
-            "progress": 1,
+            "progress": 100 / total_progress,
             "message": "ios_laminate_registration",
             "random_text": "random_text",
             "timestamp": "sdafkljhsdf"
@@ -74,23 +76,23 @@ class Neo3DRegistration:
         ios_laminate_result = self.__ios_laminate_registration(visualize=visualize)
 
         if(self.websocket is not None):
-            await self.websocket.send_json(progress_event(type="progress", progress=2, message="ios_upper_registration").get_json())            
+            await self.websocket.send_json(progress_event(type="progress", progress=100 / total_progress * 2, message="ios_upper_registration").get_json())            
             await asyncio.sleep(0.1)
         ios_upper_result = self.__ios_upper_registration()
 
         if(self.websocket is not None):
-            await self.websocket.send_json(progress_event(type="progress", progress=3, message="ios_lower_registration").get_json())
+            await self.websocket.send_json(progress_event(type="progress", progress=100 / total_progress * 3, message="ios_lower_registration").get_json())
             await asyncio.sleep(0.1)
         ios_lower_result = self.__ios_lower_registration()
 
         if(self.websocket is not None):
-            await self.websocket.send_json(progress_event(type="progress", progress=4, message="facescan_laminate_registration").get_json())
+            await self.websocket.send_json(progress_event(type="progress", progress=100 / total_progress * 4, message="facescan_laminate_registration").get_json())
             await asyncio.sleep(0.1)
         facescan_laminate_result, transformed_face_smile_mesh, type_of_facedata = self.__facescan_laminate_registration(visualize=visualize)
 
         if(type_of_facedata == "FaceScan"):
             if(self.websocket is not None):
-                await self.websocket.send_json(progress_event(type="progress", progress=5, message="facescan_rest_registration").get_json())
+                await self.websocket.send_json(progress_event(type="progress", progress=100 / total_progress * 5, message="facescan_rest_registration").get_json())
                 await asyncio.sleep(0.1)
             facescan_rest_result, facescan_retraction_result = self.__facescan_rest_registration(transformed_face_smile_mesh, facescan_laminate_result, visualize=visualize)
             facephoto_mesh = None
@@ -106,17 +108,17 @@ class Neo3DRegistration:
                                                     [0, 0, 0, 1]])
 
         if(self.websocket is not None):
-            await self.websocket.send_json(progress_event(type="progress", progress=6, message="cbct_registration").get_json())
+            await self.websocket.send_json(progress_event(type="progress", progress=100 / total_progress * 6, message="cbct_registration").get_json())
             await asyncio.sleep(0.1)
         cbct_result = self.__cbct_registration()
 
         if(self.websocket is not None):
-            await self.websocket.send_json(progress_event(type="progress", progress=7, message="ios_bow_registration").get_json())
+            await self.websocket.send_json(progress_event(type="progress", progress=100 / total_progress * 7, message="ios_bow_registration").get_json())
             await asyncio.sleep(0.1)
         ios_bow_result = self.__ios_bow_registration(ios_laminate_result, visualize=visualize)
         
         if(self.websocket is not None):
-            await self.websocket.send_json(progress_event(type="progress", progress=8, message="condyle_registration").get_json())
+            await self.websocket.send_json(progress_event(type="progress", progress=100 / total_progress * 8, message="condyle_registration").get_json())
             await asyncio.sleep(0.1)
         condyle_result = self.__condyle_detection(facescan_laminate_result, visualize)
         print(f'__condyle_detection result: {condyle_result}')
@@ -126,6 +128,7 @@ class Neo3DRegistration:
         )
         
         if(self.websocket is not None):
+            await self.websocket.send_json(progress_event(type="progress", progress=100, message="All registration completed").get_json())
             await self.websocket.send_json(progress_event(type="result", progress=100, message=result).get_json())
             await asyncio.sleep(0.1)
         return result
