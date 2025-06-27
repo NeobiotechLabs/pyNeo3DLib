2bdd304318674affa3d9e1200a2491bdd5a1ed3d
hoki <hk.sim@neobiotech.com>
2025-06-27 17:39:20 +0900

    added tf-keras

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setup(
         "pyvista",
         "imageio",
         "trimesh",
-        "retina-face"
+        "retina-face",
+        "tf-keras"
     ],
     author="NeoBiotech",
     description="3D 데이터 처리를 위한 파이썬 라이브러리",
