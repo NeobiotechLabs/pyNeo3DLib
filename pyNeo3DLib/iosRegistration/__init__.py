8ff0256c583634fa668ed9333b5ea8ce5d5beca9
sjchoi-96 <sj.choi@neobiotech.com>
2025-12-12 15:44:40 +0900

    ios 초기 배치 부분 코드 분리

--- pyNeo3DLib/iosRegistration/__init__.py
+++ pyNeo3DLib/iosRegistration/__init__.py
@@ -1,4 +1,17 @@
 from .iosLaminateRegistration import *
 from .iosAlignment import *
+from .ios_transformation import (
+    IOSTransformationCalculator,
+    IOSTransformationConstants,
+    compute_principal_axes_from_vertices,
+    compute_minimum_variance_axis_from_vertices
+)
 
-__all__ = ["IOSLaminateRegistration", "IosAlignment"] 
+__all__ = [
+    "IOSLaminateRegistration", 
+    "IosAlignment",
+    "IOSTransformationCalculator",
+    "IOSTransformationConstants",
+    "compute_principal_axes_from_vertices",
+    "compute_minimum_variance_axis_from_vertices"
+] 
